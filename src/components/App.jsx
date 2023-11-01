585314d71678424b9eacfbe8e1ea588f678442c1
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-31 20:08:43 -1100

    images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from '../App.module.css';
 import axios from 'axios';
 import Searchbar from './Searchbar/Serachbar';
@@ -10,98 +10,82 @@ import Modal from './Modal/Modal';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39459893-bedb370270db67e1c0c9a6273';
 
-export default class App extends Component {
-  state = {
-    images: [],
-    searchQuery: '',
-    page: 1,
-    isLoading: false,
-    showModal: false,
-    largeImageURL: '',
-    totalHits: 0,
-  };
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [totalHits, setTotalHits] = useState(0);
+  const [per_page, setPer_page] = useState(12);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchImages();
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.code === 'Escape' && showModal) {
+        closeModal();
+      }
     }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [showModal]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      fetchImages();
+    }
+  }, [searchQuery]);
+
+  function onChangeQuery(query) {
+    setSearchQuery(query);
+    setPage(1);
+    setPer_page(12);
+    setImages([]);
   }
 
-  onChangeQuery = query => {
-    this.setState({
-      searchQuery: query,
-      page: 1,
-      per_page: 12,
-      images: [],
-    });
-  };
-
-  fetchImages = () => {
-    const { searchQuery, page, per_page } = this.state;
-
+  function fetchImages() {
     const options = {
       method: 'GET',
       url: `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&per_page=${per_page}`,
     };
 
-    this.setState({ isLoading: true });
+    setIsLoading(true);
 
     axios(options)
       .then(response => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.data.hits],
-          totalHits: response.data.totalHits,
-          page: prevState.page + 1,
-        }));
+        setImages(prevImages => [...prevImages, ...response.data.hits]);
+        setTotalHits(response.data.totalHits);
+        setPage(prevPage => prevPage + 1);
       })
       .catch(error => console.log(error))
-      .finally(() => this.setState({ isLoading: false }));
-  };
-
-  handleImageClick = largeImageURL => {
-    this.setState({
-      showModal: true,
-      largeImageURL,
-    });
-  };
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape' && this.state.showModal) {
-      this.closeModal();
-    }
-  };
-
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-      largeImageURL: '',
-    });
-  };
-
-  render() {
-    const { images, isLoading, showModal, largeImageURL, totalHits } =
-      this.state;
-
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.onChangeQuery} />
-        <ImageGallery images={images} onImageClick={this.handleImageClick} />
-        {isLoading && <Loader />}
-        {images.length > 0 && images.length < totalHits && !isLoading && (
-          <Button onClick={this.fetchImages} />
-        )}
-        {showModal && (
-          <Modal largeImageURL={largeImageURL} onClose={this.closeModal} />
-        )}
-      </div>
-    );
+      .finally(() => setIsLoading(false));
   }
+
+  function handleImageClick(largeImageURL) {
+    setShowModal(true);
+    setLargeImageURL(largeImageURL);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+    setLargeImageURL('');
+  }
+
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={onChangeQuery} />
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {isLoading && <Loader />}
+      {images.length > 0 && images.length < totalHits && !isLoading && (
+        <Button onClick={fetchImages} />
+      )}
+      {showModal && (
+        <Modal largeImageURL={largeImageURL} onClose={closeModal} />
+      )}
+    </div>
+  );
 }
--- src/components/Searchbar/Serachbar.jsx
+++ src/components/Searchbar/Serachbar.jsx
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import css from './Searchbar.module.css';
 
-export default class Searchbar extends Component {
-  state = {
-    query: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
 
-  handleInputChange = e => {
-    this.setState({ query: e.target.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
-  };
-
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchForm_button}>
-            <span className={css.button_label}>Search</span>
-          </button>
-          <input
-            className={css.SearchForm_input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
+  function handleInputChange(e) {
+    setQuery(e.target.value);
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(query);
+    setQuery('');
+  }
+
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchForm_button}>
+          <span className={css.button_label}>Search</span>
+        </button>
+        <input
+          className={css.SearchForm_input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
 }
